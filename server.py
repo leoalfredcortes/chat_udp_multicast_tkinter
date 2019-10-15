bb79544ee122ee3f8da68b78a074ef425d050f05
leoalfredcortes <leo_1813@hotmail.com>
2019-10-14 22:13:39 -0500

    Chat udo multicast errorres

--- server.py
+++ server.py
@@ -1,32 +1,36 @@
-######################################################################################
-# Instituto Tecnológico de Colima - División de Estudios de Posgrado e Investigación #
-# Maestría en Sistemas Computacionales - Materia: Tecnologías de Internet            #
-#                                                                                    #
-# Código Fuente para Servidor Multicast UDP                                          #
-# Realizado por:                                                                     #
-# Osvaldo Vladimir Rodríguez Leal                                                    #
-# José Alfredo Cortés Quiroz                                                         #
-# Villa de Álvarez, Col 13/10/19                                                     #
-######################################################################################
-from socket import AF_INET, socket, SOCK_STREAM
+import socket
 from threading import Thread
 
 
 def accept_incoming_connections():
     #
     while True:
-        client, client_address = SERVER.accept()
+        client, client_address = SERVER.recvfrom(1024)
+        #client, client_address = SERVER.accept()
         print("%s:%s Cliente conectado." % client_address)
-        client.send(bytes("Escribe tu nickname y enviar o enter!", "utf8"))
-        addresses[client] = client_address
+        vaaars = 'Escribe tu nombre' 
+        
+        bytesAddressPair = SERVER.recvfrom(1024) # Devuelve un objeto de bytes leído por UDP y la dirección del socket del cliente
+        message = bytesAddressPair[0]
+        address = bytesAddressPair[1]
+        print(bytesAddressPair)
+        clientMsg = "--> Client Say: {}".format(message)
+        clientIP  = "MSG FROM IP: {}".format(address)    
+        print(clientMsg) #imprimimos en pantalla el mensaje
+        print(clientIP) # imprimimos en pantalla la dirección IP de donde se envia el mensaje
+        msgFromServer = "--> You Say: " # asignamos el texto a enviar en una variable  
+        bytesToSend = str.encode(msgFromServer) # Convertimos  a bytes para el envío
+        SERVER.sendto(bytesToSend, clientIP) # Enviamos mensaje a cliente
+
+        addresses[clientIP] = client_address
         Thread(target=saludo_mano, args=(client,)).start()
 
 
-def saludo_mano(client):  # Takes client socket as argument.
+def saludo_mano():  # Takes client socket as argument.
     """Handles a single client connection."""
 
     name = client.recv(BUFSIZ).decode("utf8")
-    welcome = 'Bienvenido %s! Siquieres cerrar el chat escribe, -> {quit} <- para salir.' % name
+    welcome = 'Bienvenido %s! Siquieres cerrar el chat escribe, ->   {quit} <- para salir.' % name
     client.send(bytes(welcome, "utf8"))
     msg = "%s a entrado al chat!" % name
     broadcast(bytes(msg, "utf8"))
@@ -54,19 +58,15 @@ def broadcast(msg, prefix=""):  # prefix is for name identification.
 clients = {}
 addresses = {}
 
-HOST = '127.0.0.1'
+HOST = "127.0.0.1"
 PORT = 33000
 BUFSIZ = 1024
 ADDR = (HOST, PORT)
 #se inicializa el servidor por medio del socket como stream data
-SERVER = socket()
+SERVER = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 #se hace el enlace con la direccion y puerto
 SERVER.bind(ADDR)
 #el servidor se mantiene a la escucha de algun cliente por medio del enlace previamente creado
 if __name__ == "__main__":
-    SERVER.listen(5)
-    print("Eserando conexion de clientes...")
-    ACCEPT_THREAD = Thread(target=accept_incoming_connections)
-    ACCEPT_THREAD.start()
-    ACCEPT_THREAD.join()
-    SERVER.close()
+   print("Eserando conexion de clientes...")
+   accept_incoming_connections()
